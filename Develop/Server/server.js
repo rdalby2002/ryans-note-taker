7c162a9ebe718e0f1d18cb40c00b94ee011356a1
rdalby2002 <ryancdalby@gmail.com>
2023-01-03 21:30:59 -0600

    wrote basic GET routes in server.js, and created helper functions to be used in POST route

---
+++ Develop/Server/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const path = require("path");
+const uuid = require("../helpers/uuid");
+const clog = require("../middleware/clog");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(clog);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.get('/api/notes', (req, res) => res.json(db.json));
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
